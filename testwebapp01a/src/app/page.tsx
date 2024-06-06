259c95cb241cba47d4a54008ca1aa532283a7e3a
southasian <43087416+southasian@users.noreply.github.com>
2024-06-06 20:44:36 +0530

    added webappp for next.js

---
+++ testwebapp01a/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <div>
+        <h2>(test-web-app01a)</h2>
+      </div>
+    </div>
+  );
+}
